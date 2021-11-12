5a3baae95c42b62f1d8bfa056ae7f25be47be0e9
Jon Wen <jswen19109814@gmail.com>
2021-11-12 17:08:15 -0500

    Reconfigure admin

--- src/main/java/com/ss/utopia/UtopiaApplication.java
+++ src/main/java/com/ss/utopia/UtopiaApplication.java
@@ -4,22 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class UtopiaApplication extends SpringBootServletInitializer {
 	
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(UtopiaApplication.class);
 	}
 	public static void main(String[] args) {
-//		SpringApplication.run(UtopiaApplication.class, args);
-	}
-	@RequestMapping(value = "/")
-	public String test() {
-		return "Tester";
+		SpringApplication.run(UtopiaApplication.class, args);
 	}
 }
